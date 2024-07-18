8fe36c82bbdbce2117cf5221c2a7a7da8ee59a3b
AmrIbrahim <afadl@worldofss.com>
2024-07-18 23:20:09 +0300

    remove banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       home: const HomePage(),
